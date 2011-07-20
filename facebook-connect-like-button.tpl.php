18cb0d80b699e149b8fe30317c7286488667141c
Dennis Stevense <dennis@naquah.net>
2011-07-20 14:57:33 +0200

    Made like button block not use the current URL but a customizable site-wide URL.

--- facebook-connect-like-button.tpl.php
+++ facebook-connect-like-button.tpl.php
@@ -2,5 +2,5 @@
 // Developed by Dennis Stevense for Digital Deployment
 ?>
 <div class="facebook-connect-like-button">
-  <fb:like href="<?php print $like_url; ?>" send="false" layout="button_count" width="120" show_faces="false"></fb:like>
+  <fb:like href="<?php print !empty($like_url) ? $like_url : ""; ?>" send="false" layout="button_count" width="120" show_faces="false"></fb:like>
 </div>
