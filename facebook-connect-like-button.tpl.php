3ac5c0df5a00c25bd4f1aec97f6cfa51e4a0986e
Dennis Stevense <dennis@naquah.net>
2011-07-20 17:03:37 +0200

    Include send button on teasers.

--- facebook-connect-like-button.tpl.php
+++ facebook-connect-like-button.tpl.php
@@ -2,5 +2,5 @@
 // Developed by Dennis Stevense for Digital Deployment
 ?>
 <div class="facebook-connect-like-button">
-  <fb:like href="<?php print !empty($like_url) ? $like_url : ""; ?>" send="false" layout="button_count" width="120" show_faces="false"></fb:like>
+  <fb:like href="<?php print $fb_href; ?>" send="<?php print $fb_send; ?>" layout="button_count" width="120" show_faces="false"></fb:like>
 </div>
