70a87567b34dff89c91bb049265db9e42d0a7ae0
Dennis Stevense <dennis@naquah.net>
2011-07-20 17:36:58 +0200

    Allow Drupal comments to be replaced by Facebook comments.

---
+++ facebook-connect-comments.tpl.php
@@ -0,0 +1,6 @@
+<?php
+// Developed by Dennis Stevense for Digital Deployment
+?>
+<div class="facebook-connect-comments">
+  <fb:comments href="<?php print $fb_href; ?>" num_posts="2" width="500"></fb:comments>
+</div>
